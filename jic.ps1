51e04dc54e9a9d9fa7f256fe8fbd87c513f9d412
kcrkor <kcrkor@users.noreply.github.com>
2023-04-25 16:44:10 +0100

    JIC start

---
+++ jic.ps1
@@ -0,0 +1,7 @@
+Set-ExplorerOptions -showFileExtensions
+#Enable-RemoteDesktop
+
+choco install googlechrome
+
+#choco install Microsoft-Hyper-V-All -source windowsFeatures
+#choco install IIS-WebServerRole -source windowsfeatures
