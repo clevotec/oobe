327cd3be548342b95dc18e188f16a5a24d653f04
kcrkor <kcrkor@users.noreply.github.com>
2023-04-29 17:56:02 +0100

    Updates jic.ps1

--- jic.ps1
+++ jic.ps1
@@ -1,7 +1,16 @@
-Set-ExplorerOptions -showFileExtensions
+#Set-ExplorerOptions -showFileExtensions
 #Enable-RemoteDesktop
 
+choco install 7zip
 choco install googlechrome
+choco install microsoft-office-deployment --params '/64bit /Product:ProPlus2021Volume'
+choco install brave
+choco install adobereader
+choco install microsoft-edge
+choco install keepass
+choco install keepass-rpc
+choco install vlc
+choco install choco-upgrade-all-at --params '/DAILY:yes /TIME:04:00 /ABORTTIME:08:00'
 
 #choco install Microsoft-Hyper-V-All -source windowsFeatures
 #choco install IIS-WebServerRole -source windowsfeatures
