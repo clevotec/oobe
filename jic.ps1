5d77ee99168b2d8684ce520b248f6c1f390d2d4b
Chris Kortschak <chris@kortschak.org>
2023-05-01 15:10:49 +0100

    JIC updates

--- jic.ps1
+++ jic.ps1
@@ -1,5 +1,8 @@
-#Set-ExplorerOptions -showFileExtensions
-#Enable-RemoteDesktop
+# Boxstarter Winconfig
+Disable-BingSearch
+Disable-GameBarTips
+Set-ExplorerOptions -showFileExtensions -showHiddenFilesAndFolders
+# Set-BoxstarterTaskbarOptions -DisableSearchBox
 
 choco install 7zip
 choco install googlechrome
@@ -11,6 +14,30 @@ choco install keepass
 choco install keepass-rpc
 choco install vlc
 choco install choco-upgrade-all-at --params '/DAILY:yes /TIME:04:00 /ABORTTIME:08:00'
+choco install micrsoft-teams.install
 
-#choco install Microsoft-Hyper-V-All -source windowsFeatures
-#choco install IIS-WebServerRole -source windowsfeatures
+# Onedrive Setup
+$HKLMregistryPath = 'HKLM:\SOFTWARE\Policies\Microsoft\OneDrive' ##Path to HKLM keys
+$TenantGUID = 'd911a68a-30cf-4719-80e6-dab4d56bfc93'
+
+if(!(Test-Path $HKLMregistryPath)){New-Item -Path $HKLMregistryPath -Force}
+
+New-ItemProperty -Path $HKLMregistryPath -Name 'SilentAccountConfig' -Value '1' -PropertyType DWORD -Force | Out-Null ##Enable silent account configuration
+New-ItemProperty -Path $HKLMregistryPath -Name 'DisablePersonalSync' -Value '1' -PropertyType DWORD -Force | Out-Null ##Disable personal OneDrive
+New-ItemProperty -Path $HKLMregistryPath -Name 'FilesonDemandEnabled' -Value '1' -PropertyType DWORD -Force | Out-Null ##Enable Files on Demand
+New-ItemProperty -Path $HKLMregistryPath -Name 'KFMSilentOptInWithNotification' -Value $TenantGUID -PropertyType String -Force | Out-Null ##Enable KFM with notification
+
+
+# Windows Hello for Business
+$HKLMregistryPath = 'HKLM:\SOFTWARE\Policies\Microsoft\PassportForWork' ##Path to HKLM keys
+New-ItemProperty -Path $HKLMregistryPath -Name 'Enabled' -Value '1' -PropertyType DWORD -Force | Out-Null ##Enable Windows Hello for Business
+New-ItemProperty -Path $HKLMregistryPath -Name 'DisablePostLogonProvisioning' -Value '1' -PropertyType DWORD -Force | Out-Null ##Disable post logon provisioning
+# New-ItemProperty -Path $HKLMregistryPath -Name 'RequirePinForSignIn' -Value '1' -PropertyType DWORD -Force | Out-Null ##Require PIN for sign in
+# New-ItemProperty -Path $HKLMregistryPath -Name 'UsePreferredBiometric' -Value '1' -PropertyType DWORD -Force | Out-Null ##Use preferred biometric
+
+# Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\MicrosoftEdge\SearchScopes" -Name "ShowSearchSuggestionsGlobal" -Value 1
+
+
+# Install Windows Updates
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -AcceptEula
